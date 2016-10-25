c69ae4bf099ab5cfd9f553ec19de620600cb5061
nikolai <n.ivanov.p@gmail.com>
2016-10-25 22:57:26 +0300

    test-1

--- test/mysql.unit.spec.js
+++ test/mysql.unit.spec.js
@@ -8,7 +8,7 @@ describe('mysql test', () => {
     before(() => {
         connection = mysql.createConnection({
             host     : '127.0.0.1',
-            user     : 'root',
+            user     : 'my_user',
             password : 'admin',
             database : 'nodejs'
         });
