ba19e67f8f977e9396105e645c7f4d937973402e
nikolai <n.ivanov.p@gmail.com>
2016-10-25 15:22:13 +0300

    init commit

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var mysql      = require('mysql');
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'admin',
+    database : 'nodejs'
+});
+
+var QUERY_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS `names` (`id` int(10) unsigned NOT NULL auto_increment, `name` varchar(50) NOT NULL default '0', PRIMARY KEY  (`id`))";
+
+var SELECT_FROM = `SELECT * from names`;
+
+connection.connect();
+
+connection.query(QUERY_CREATE_TABLE, function(err, result) {
+    if (!err)
+        console.log('The solution is: ', result);
+    else
+        console.log('Error while performing Query.', err);
+});
+
+connection.query(SELECT_FROM, function(err, rows) {
+    if (!err)
+        console.log('The solution is: ', rows);
+    else
+        console.log('Error while performing Query.');
+});
+
+connection.end();
---
+++ test/mysql.unit.spec.js
@@ -0,0 +1,35 @@
+"use strict";
+
+var mysql      = require('mysql');
+
+describe('mysql test', () => {
+    var connection;
+
+    before(() => {
+        connection = mysql.createConnection({
+            host     : 'localhost',
+            user     : 'root',
+            password : 'admin',
+            database : 'nodejs'
+        });
+    });
+
+    it('test connection', (done) => {
+        connection.connect(function(err) {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                done(err);
+            }
+            console.log('connected as id ' + connection.threadId);
+            done();
+        });
+    });
+
+    it('insert new item to mysql table', () => {
+
+    });
+
+    it('select all items', () => {
+
+    });
+});
